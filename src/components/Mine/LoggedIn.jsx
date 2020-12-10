53226de9a0f6c06c884ea37d9eb3dafff612883f
SeekingMini <SeekingMini@gmail.com>
2020-12-10 15:43:46 +0800

    优化【个人中心】模块界面样式

--- src/components/Mine/LoggedIn.jsx
+++ src/components/Mine/LoggedIn.jsx
@@ -3,11 +3,30 @@ import React from 'react';
 
 // Redux 
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+// Redux Action
+import * as serverActions from '../../utils/network';
+import * as authActions from '../../redux/actions/auth';
+import * as dashboardActions from '../../redux/actions/dashboard';
+import * as dataTableActions from '../../redux/actions/dataTable';
+import * as headerActions from '../../redux/actions/header';
+import * as urlConfigActions from '../../redux/actions/urlConfig';
 
 // Ant Design组件库
 import 'antd/dist/antd.css';
-import { Row, Col, Card } from "antd";
+import { SettingOutlined } from '@ant-design/icons';
+import { Row, Col, Card, Avatar, Dropdown, Menu, Button, message } from "antd";
 
+// Util
+import urls from '../../constants/urls';
+import messageType from '../../constants/messageType';
+
+// Asset
+import avatar from '../../asset/avatar.png';
+import background from '../../asset/background.png';
+
+const { Meta } = Card;
 
 class LoggedIn extends React.Component {
 
@@ -15,22 +34,87 @@ class LoggedIn extends React.Component {
         super(props);
     }
 
+    onClickMenuItem = ({ key }) => {
+        switch (key) {
+            case 'logout':
+                this.logout();
+
+            default:
+                break
+        }
+    };
+
+    logout = () => {
+        const loading = message.loading(messageType.Loading.LOGGING_OUT, 0);
+
+        this.props.serverActions.logoutRequest(urls.user_logout)
+            .then(resp => {
+                setTimeout(loading, 1);
+                return resp.data;
+            })
+            .then(data => {
+                // 退出成功
+                if (data.code == 200) {
+                    this.props.authActions.logout();
+                    // 清除相关信息
+                    this.clearUserInfo();
+
+                    message.success(messageType.Success.LOGOUT_OK, 1.0);
+                }
+                // 退出失败
+                else {
+                    message.error(messageType.Error.LOGOUT_FAIL, 1.0);
+                };
+            })
+            .catch(() => {
+                setTimeout(loading, 1);
+                message.error(messageType.Error.ERROR_HAPPEN, 1.0);
+                // 发生错误时还是要退出
+                this.props.authActions.logout();
+                // 清除相关信息
+                this.clearUserInfo();
+            });
+    }
+
+    clearUserInfo = () => {
+        localStorage.removeItem('loginTicket');
+        localStorage.removeItem('username');
+        localStorage.removeItem('password');
+        this.props.dashboardActions.clear();
+        this.props.headerActions.clear();
+        this.props.dataTableActions.clear();
+        this.props.urlConfigActions.clear();
+    }
+
     render() {
+        const menu = (
+            <Menu onClick={this.onClickMenuItem}>
+                <Menu.Item key="logout" style={{ textAlign: 'center' }}>退出登录</Menu.Item>
+            </Menu>
+        );
+
         return (
             <>
-                <Row style={{height: 200}}>
+                <Row style={{ height: 100 }}>
                     <Col span={24}></Col>
                 </Row>
-                <Row gutter={16}>
-                    <Col span={10}></Col>
-                    <Col span={4}>
-                        <Card title="当前用户"
-                            bordered
-                            style={{ textAlign: 'center' }}>
-                            {this.props.username}
+                <Row>
+                    <Col span={9}></Col>
+                    <Col span={8}>
+                        <Card
+                            style={{ width: 300 }}
+                            cover={<img alt="example" src={background} />}
+                            actions={[
+                                <Dropdown trigger='click' overlay={menu}>
+                                    <SettingOutlined key="setting" />
+                                </Dropdown>]} >
+                            <Meta
+                                avatar={<Avatar src={avatar} shape='square' />}
+                                title={this.props.username}
+                                description='系统管理员' />
                         </Card>
                     </Col>
-                    <Col span={10}></Col>
+                    <Col span={7}></Col>
                 </Row>
                 <Row>
                     <Col span={24}></Col>
@@ -43,9 +127,19 @@ class LoggedIn extends React.Component {
 
 const mapStateToProps = (state) => {
     return {
-        loggedIn: state.auth.loggedIn,
         username: state.auth.username
     }
 }
 
-export default connect(mapStateToProps, null)(LoggedIn);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        serverActions: bindActionCreators(serverActions, dispatch),
+        authActions: bindActionCreators(authActions, dispatch),
+        headerActions: bindActionCreators(headerActions, dispatch),
+        dataTableActions: bindActionCreators(dataTableActions, dispatch),
+        urlConfigActions: bindActionCreators(urlConfigActions, dispatch),
+        dashboardActions: bindActionCreators(dashboardActions, dispatch)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoggedIn);
--- src/components/Mine/MinePage.jsx
+++ src/components/Mine/MinePage.jsx
@@ -1,22 +1,10 @@
 // React
 import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
-
-// Redux
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-
-// Redux Action
-import * as serverActions from '../../utils/network';
-import * as authActions from '../../redux/actions/auth';
-import * as dashboardActions from '../../redux/actions/dashboard';
-import * as dataTableActions from '../../redux/actions/dataTable';
-import * as headerActions from '../../redux/actions/header';
-import * as urlConfigActions from '../../redux/actions/urlConfig';
+import { withRouter } from 'react-router-dom';
 
 // Ant Design组件库
 import 'antd/dist/antd.css';
-import { Layout, Menu, message } from "antd";
+import { Layout } from "antd";
 
 // 自定义组件
 import LoggedIn from './LoggedIn';
@@ -24,88 +12,17 @@ import LoggedIn from './LoggedIn';
 // css
 import './MinePage.css';
 
-// Constants
-import urls from '../../constants/urls';
-import messageType from '../../constants/messageType';
-
 const { Content } = Layout;
 
 class MinePage extends React.Component {
 
     constructor(props) {
         super(props);
-
-        this.userLinks = [
-            {
-                key: "退出",
-                title: "退出",
-                link: '/'
-            },
-        ]
-
-    }
-
-    // 点击菜单栏触发的回调
-    onClickMenuItem = () => {
-        const loading = message.loading(messageType.Loading.LOGGING_OUT, 0);
-        this.props.serverActions.logoutRequest(urls.user_logout)
-            .then(resp => {
-                setTimeout(loading, 1);
-                return resp.data;
-            })
-            .then(data => {
-                // 退出成功
-                if (data.code == 200) {
-                    this.props.authActions.logout();
-                    // 清除相关信息
-                    this.clearUserInfo();
-
-                    message.success(messageType.Success.LOGOUT_OK, 1.0);
-                }
-                // 退出失败
-                else {
-                    message.error(messageType.Error.LOGOUT_FAIL, 1.0);
-                };
-            })
-            .catch(err => {
-                setTimeout(loading, 1);
-                message.error(messageType.Error.ERROR_HAPPEN, 1.0);
-                // 发生错误时还是要退出
-                this.props.authActions.logout();
-                // 清除相关信息
-                this.clearUserInfo();
-            })
-    }
-
-    clearUserInfo = () => {
-        localStorage.removeItem('loginTicket');
-        localStorage.removeItem('username');
-        localStorage.removeItem('password');
-        this.props.dashboardActions.clear();
-        this.props.headerActions.clear();
-        this.props.dataTableActions.clear();
-        this.props.urlConfigActions.clear();
     }
 
     render() {
         return (
             <Content style={{ margin: '15px 15px 15px 15px' }}>
-                {/* 导航栏 */}
-                <Menu
-                    theme="light"
-                    mode="horizontal"
-                    defaultSelectedKeys={['1']}
-                    onClick={this.onClickMenuItem}>
-                    {
-                        this.userLinks.map(item => {
-                            return (
-                                <Menu.Item key={item.key}>
-                                    <Link to={item.link}>{item.title}</Link>
-                                </Menu.Item>
-                            )
-                        })
-                    }
-                </Menu>
                 {/* 内容 */}
                 <LoggedIn />
             </Content>
@@ -114,21 +31,4 @@ class MinePage extends React.Component {
 
 }
 
-const mapStateToProps = (state) => {
-    return {
-        loggedIn: state.auth.loggedIn
-    }
-}
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        serverActions: bindActionCreators(serverActions, dispatch),
-        authActions: bindActionCreators(authActions, dispatch),
-        headerActions: bindActionCreators(headerActions, dispatch),
-        dataTableActions: bindActionCreators(dataTableActions, dispatch),
-        urlConfigActions: bindActionCreators(urlConfigActions, dispatch),
-        dashboardActions: bindActionCreators(dashboardActions, dispatch)
-    }
-}
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(MinePage));
+export default withRouter(MinePage);
